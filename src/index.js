1ba07cbcd10e5a82db83a852322ab4fe1f0d7c1b
Mário Cesar <mc.fialho@hotmail.com>
2023-04-07 23:38:08 -0300

    feat: documentation with swagger

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import connectDatabase from './src/database/db.js';
+import connectDatabase from './database/db.js';
 import dotenv from 'dotenv';
 
-import userRoute from "./src/routes/user.route.js";
-import authRoute from "./src/routes/auth.route.js";
-import newsRoute from "./src/routes/news.route.js";
+import userRoute from "./routes/user.route.js";
+import authRoute from "./routes/auth.route.js";
+import newsRoute from "./routes/news.route.js";
+import swaggerRoute from "./routes/swagger.route.js";
 
 dotenv.config();
 
@@ -16,5 +17,6 @@ app.use(express.json());
 app.use('/user', userRoute);
 app.use('/auth', authRoute);
 app.use('/news', newsRoute);
+app.use("/doc", swaggerRoute);
 
 app.listen(port, () => console.log(`Server listen on port ${port}`));
