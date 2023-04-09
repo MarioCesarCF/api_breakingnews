dd662d4d951573d8c2094ee8989344199f0e71a2
Mário Cesar <mc.fialho@hotmail.com>
2023-04-09 15:46:00 -0300

    refactor: swagger.route

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import express from 'express';
 import connectDatabase from './database/db.js';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 import userRoute from "./routes/user.route.js";
 import authRoute from "./routes/auth.route.js";
 import newsRoute from "./routes/news.route.js";
-import swaggerRoute from "./routes/swagger.route.js";
+import swaggerRoute from "./routes/swagger.route.cjs";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 27017;
+app.use(cors());
 
 connectDatabase();
 app.use(express.json());
